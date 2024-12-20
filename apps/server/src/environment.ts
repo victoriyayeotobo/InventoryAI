159098a2e1b3cfc72525a37764817334f7b1043d
maceteligolden <maceteligolden@gmail.com>
2024-12-20 13:38:09 +0100

    feat:add env file

---
+++ apps/server/src/environment.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+export const env = {
+    PORT: process.env.PORT || 5000,
+    DB: process.env.MONGO_DB_URI || "mongodb://127.0.0.1:27017/inventoryAI"
+}
